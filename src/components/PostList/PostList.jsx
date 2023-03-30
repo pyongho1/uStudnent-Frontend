64febb488c4327aad6d43324690041cf15bb11ee
Yong Park <pyongho0306@gmail.com>
2023-03-29 17:33:13 -0700

    Clean up table for landing page

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -2,48 +2,35 @@
 import styles from "./PostList.module.css";
 
 // Bootstrap
-import Table from 'react-bootstrap/Table';
+import Table from "react-bootstrap/Table";
 
-const PostList = ({ post, user }) => {
+const PostList = ({ post, index, user }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.colContainer}>
-        <h4>{post.title}</h4>
-        {/* <p>{post.content}</p> */}
+      {/* <h4>{post.title}</h4>
+        <p>{post.content}</p>
         <div className={styles.infoBox}>
-          <p>{post.author.name}</p>
-          <p>{post.createdAt}</p>
-        </div>
-        <Table striped>
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Username</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <td>2</td>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <td>3</td>
-          <td colSpan={2}>Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </Table>
-      </div>
+        <p>{post.author.name}</p>
+        <p>{post.createdAt}</p>
+      </div> */}
+      <Table striped>
+        {/* <thead>
+            <tr>
+            <th>#</th>
+            <th>Author</th>
+            <th>Title</th>
+            <th>Date Posted</th>
+            </tr>
+          </thead> */}
+        <tbody>
+          <tr>
+            <td>{index + 1}</td>
+            <td>{post.author.name}</td>
+            <td>{post.title}</td>
+            <td>{post.createdAt.substring(0, 10)}</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 };
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,6 +1,9 @@
 // Styles
 import styles from "./Landing.module.css";
 
+// Bootstrap
+import Table from "react-bootstrap/Table";
+
 // Components
 import PostList from "../../components/PostList/PostList";
 
@@ -10,8 +13,26 @@ const Landing = ({ user, posts }) => {
       <div className={styles.container}>
         <div className={styles.contentContainer}>
           <h1>hello, {user ? user.name : "friend"}</h1>
-          {posts.map((post) => (
-            <PostList key={post.title} post={post} user={user} />
+          {posts.map((post, index) => (
+            <PostList key={post.title} post={post} index={index} user={user} />
+            // <Table striped>
+            //   <thead>
+            //     <tr>
+            //       <th>#</th>
+            //       <th>Author</th>
+            //       <th>Title</th>
+            //       <th>Date Posted</th>
+            //     </tr>
+            //   </thead>
+            //   <tbody>
+            //     <tr>
+            //       <td>{index + 1}</td>
+            //       <td>{post.author.name}</td>
+            //       <td>{post.title}</td>
+            //       <td>{post.createdAt}</td>
+            //     </tr>
+            //   </tbody>
+            // </Table>
           ))}
         </div>
       </div>
