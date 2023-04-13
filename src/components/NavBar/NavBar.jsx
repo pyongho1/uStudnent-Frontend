f7cef2a71d83e33294e66f1aec830802bbc3746a
Yong Park <pyongho0306@gmail.com>
2023-04-12 23:42:48 -0700

    Edit styling for create

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,8 +8,16 @@ const NavBar = ({ user, handleLogout }) => {
         <>
           {/* <p className="userName">Welcome, {user.name}</p> */}
 
+          <Link to="/" className="navLists">
+            홈
+          </Link>
+
           <Link to="/sell" className="navLists">
             사고팔고
+          </Link>
+
+          <Link to="/sell" className="navLists">
+            룸메찾아유
           </Link>
 
           <Link to="/new" className="navLists">
